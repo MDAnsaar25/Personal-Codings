1cae9625faf767b6be9fb98ac0ca9837b3c2cad9
Mohamed Ansaar <59823784+MDAnsaar25@users.noreply.github.com>
2020-06-04 20:15:10 +0530

    Create Calculation on Marks.py

---
+++ Calculation on Marks.py
@@ -0,0 +1,11 @@
+a = float(input("Enter the English mark"))
+b = float(input("Enter the tamil mark"))
+c = float(input("Enter the Maths mark"))
+d = float(input("Enter the science mark"))
+e = float(input("Enter the social mark"))
+PR = (a+b+c+d+e)/100
+TO = (a+b+c+d+e)
+AV = (TO/500)*100
+print ("The Average is :" +str(AV))
+print ("The Total is :" +str(TO))
+print ("The Percentage is :" +str(PR))
